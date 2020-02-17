ca84ca5be58d29f36f8f3c7db3aaf0f84e4f4bac
Marc MOREAU <moreau.marc.web@gmail.com>
2020-02-18 00:07:17 +0100

    n/a

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const path = require('path');
+const serveStatic = require('serve-static');
+const app = express();
+const portFinder = require('portfinder');
+
+portFinder.getPort({
+    port: 8080,    // minimum port
+    stopPort: 9080 // maximum port
+}, (error, portFound) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    const port = process.env.PORT || portFound;
+
+    app.use(history({
+        verbose: true
+    }));
+
+    app.use(serveStatic(path.join(__dirname, '/dist')));
+
+    app.listen(port, () => {
+        console.log('Server started at http://localhost:%s', port)
+    });
+});
